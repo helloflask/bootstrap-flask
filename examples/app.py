05be3a0b71c99bda6dad539285522cc8476dd7c4
Grey Li <withlihui@gmail.com>
2018-06-11 18:19:21 +0800

    Complete all the example pages

--- examples/app.py
+++ examples/app.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField
 from wtforms.validators import DataRequired, Length
 
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'dev'
 
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
 
 
 class HelloForm(FlaskForm):
@@ -20,10 +22,13 @@ class HelloForm(FlaskForm):
     submit = SubmitField()
 
 
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = HelloForm()
-    return render_template('index.html', form=form)
+    return render_template('index.html')
 
 
 @app.route('/form', methods=['GET', 'POST'])
@@ -34,17 +39,23 @@ def test_form():
 
 @app.route('/nav', methods=['GET', 'POST'])
 def test_nav():
-    form = HelloForm()
-    return render_template('nav.html', form=form)
+    return render_template('nav.html')
 
 
 @app.route('/pagination', methods=['GET', 'POST'])
 def test_pagination():
-    form = HelloForm()
-    return render_template('pagination.html', form=form)
+    db.drop_all()
+    db.create_all()
+    for i in range(100):
+        m = Message()
+        db.session.add(m)
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    pagination = Message.query.paginate(page, per_page=10)
+    messages = pagination.items
+    return render_template('pagination.html', pagination=pagination, messages=messages)
 
 
 @app.route('/utils', methods=['GET', 'POST'])
 def test_utils():
-    form = HelloForm()
-    return render_template('utils.html', form=form)
+    return render_template('utils.html')
