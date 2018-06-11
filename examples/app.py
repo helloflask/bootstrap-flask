210cbc136edfa26bfcea43ec94b515acada0ce53
Grey Li <withlihui@gmail.com>
2018-06-11 17:58:03 +0800

    Add routes for demo pages

--- examples/app.py
+++ examples/app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, BooleanField
+from wtforms import StringField, SubmitField, BooleanField, PasswordField
 from wtforms.validators import DataRequired, Length
 
 from flask_bootstrap import Bootstrap
@@ -14,8 +14,8 @@ bootstrap = Bootstrap(app)
 
 
 class HelloForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(1, 20)])
-    body = TextAreaField('Message', validators=[DataRequired(), Length(1, 200)])
+    username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(8, 150)])
     remember = BooleanField('Remember me')
     submit = SubmitField()
 
@@ -24,3 +24,27 @@ class HelloForm(FlaskForm):
 def index():
     form = HelloForm()
     return render_template('index.html', form=form)
+
+
+@app.route('/form', methods=['GET', 'POST'])
+def test_form():
+    form = HelloForm()
+    return render_template('form.html', form=form)
+
+
+@app.route('/nav', methods=['GET', 'POST'])
+def test_nav():
+    form = HelloForm()
+    return render_template('nav.html', form=form)
+
+
+@app.route('/pagination', methods=['GET', 'POST'])
+def test_pagination():
+    form = HelloForm()
+    return render_template('pagination.html', form=form)
+
+
+@app.route('/utils', methods=['GET', 'POST'])
+def test_utils():
+    form = HelloForm()
+    return render_template('utils.html', form=form)
