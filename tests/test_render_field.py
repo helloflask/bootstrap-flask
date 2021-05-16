d08f8a747e25f1a5063218eab347ce79de848c5a
yuxiaoy <withyuxiaoy@gmail.com>
2021-05-16 23:00:20 +0800

    Fix csrf token label of render field macro

--- tests/test_render_field.py
+++ tests/test_render_field.py
@@ -7,11 +7,13 @@ def test_render_field(app, client, hello_form):
         form = hello_form()
         return render_template_string('''
         {% from 'bootstrap/form.html' import render_field %}
+        {{ render_field(form.csrf_token) }}
         {{ render_field(form.username) }}
         {{ render_field(form.password) }}
         ''', form=form)
 
     response = client.get('/field')
     data = response.get_data(as_text=True)
+    assert '<label class="form-control-label" for="csrf_token">CSRF Token</label>' not in data
     assert '<input class="form-control" id="username" name="username"' in data
     assert '<input class="form-control" id="password" name="password"' in data
