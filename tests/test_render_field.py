650920344afca30e35904f104afaf7de9c946dab
yuxiaoy <withyuxiaoy@gmail.com>
2021-05-17 22:17:01 +0800

    Add support for HiddenField

--- tests/test_render_field.py
+++ tests/test_render_field.py
@@ -8,6 +8,7 @@ def test_render_field(app, client, hello_form):
         return render_template_string('''
         {% from 'bootstrap/form.html' import render_field %}
         {{ render_field(form.csrf_token) }}
+        {{ render_field(form.hidden) }}
         {{ render_field(form.username) }}
         {{ render_field(form.password) }}
         ''', form=form)
@@ -15,5 +16,6 @@ def test_render_field(app, client, hello_form):
     response = client.get('/field')
     data = response.get_data(as_text=True)
     assert '<label class="form-control-label" for="csrf_token">CSRF Token</label>' not in data
+    assert '<label class="form-control-label" for="hidden">Hidden</label>' not in data
     assert '<input class="form-control" id="username" name="username"' in data
     assert '<input class="form-control" id="password" name="password"' in data
