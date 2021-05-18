a6f825659c85dc799f3cf02e2c86bb00d635b977
yuxiaoy <withyuxiaoy@gmail.com>
2021-05-18 08:25:51 +0800

    Fix csrf token label of render field macro (#131)
    * Fix csrf token label of render field macro
    
    * Add support for HiddenField

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from flask import Flask, render_template_string
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms import BooleanField, PasswordField, StringField, SubmitField, HiddenField
 from wtforms.validators import DataRequired, Length
 
 
@@ -10,6 +10,7 @@ class HelloForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
     password = PasswordField('Password', validators=[DataRequired(), Length(8, 150)])
     remember = BooleanField('Remember me')
+    hidden = HiddenField()
     submit = SubmitField()
 
 
