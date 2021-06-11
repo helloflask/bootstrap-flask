471751a3425e6038ec670e98cce3666e24341ef3
yuxiaoy <withyuxiaoy@gmail.com>
2021-06-11 22:01:53 +0800

    Update examples for custom icon title in table actions

--- examples/app.py
+++ examples/app.py
@@ -21,6 +21,12 @@ app.config['BOOTSTRAP_BTN_STYLE'] = 'primary'
 app.config['BOOTSTRAP_BTN_SIZE'] = 'sm'
 # app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'lumen'  # uncomment this line to test bootswatch theme
 
+# set default icon title of table actions
+app.config['BOOTSTRAP_TABLE_VIEW_TITLE'] = 'Read'
+app.config['BOOTSTRAP_TABLE_EDIT_TITLE'] = 'Update'
+app.config['BOOTSTRAP_TABLE_DELETE_TITLE'] = 'Remove'
+app.config['BOOTSTRAP_TABLE_NEW_TITLE'] = 'Create'
+
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
