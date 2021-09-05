3e2cb434928fa15f9741871ab6cb87c3fbab54ee
Grey Li <withlihui@gmail.com>
2021-09-05 12:52:53 +0800

    Release 1.8.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='Bootstrap-Flask',
-    version='1.7.1dev',
+    version='1.8.0',
     url='https://github.com/greyli/bootstrap-flask',
     license='MIT',
     author='Grey Li',
