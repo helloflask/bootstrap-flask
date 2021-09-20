ecb8fd065df80710a33f3dd8bf511c1dd4e457ff
Grey Li <withlihui@gmail.com>
2021-09-20 12:08:59 +0800

    Combine class arguments of render_field with Bootstrap classes

--- tests/test_render_field.py
+++ tests/test_render_field.py
@@ -19,3 +19,40 @@ def test_render_field(app, client, hello_form):
     assert '<label class="form-control-label" for="hidden">Hidden</label>' not in data
     assert '<input class="form-control" id="username" name="username"' in data
     assert '<input class="form-control" id="password" name="password"' in data
+
+
+def test_render_field_with_classes(app, client, hello_form):
+    @app.route('/render_kw_class')
+    def test_render_kw_class():
+        form = hello_form()
+        form.name.render_kw = {'class': 'render_kw_class'}
+        form.username.render_kw = {'class': 'render_kw_class'}
+        form.password.render_kw = {'class': 'render_kw_class'}
+        return render_template_string('''
+        {% from 'bootstrap/form.html' import render_field %}
+        {{ render_field(form.name) }}
+        {{ render_field(form.username, class_='test') }}
+        {{ render_field(form.password, class='test') }}
+        ''', form=form)
+
+    response = client.get('/render_kw_class')
+    data = response.get_data(as_text=True)
+    print(data)
+    assert '<input class="form-control render_kw_class" id="name" name="name"' in data
+    assert '<input class="form-control test" id="username" name="username"' in data
+    assert '<input class="form-control test" id="password" name="password"' in data
+
+    @app.route('/kwargs_class')
+    def test_kwargs_class():
+        form = hello_form()
+        return render_template_string('''
+        {% from 'bootstrap/form.html' import render_field %}
+        {{ render_field(form.username, class_='test') }}
+        {{ render_field(form.password, class='test') }}
+        ''', form=form)
+
+    response = client.get('/kwargs_class')
+    data = response.get_data(as_text=True)
+    print(data)
+    assert '<input class="form-control test" id="username" name="username"' in data
+    assert '<input class="form-control test" id="password" name="password"' in data
