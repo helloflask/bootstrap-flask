7de1ad42f21bb26a4adba8d0c239c191a1223988
Grey Li <withlihui@gmail.com>
2022-01-27 22:29:21 +0800

    Work on 2.1.0 again

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='Bootstrap-Flask',
-    version='2.0.1',
+    version='2.1.0dev',
     url='https://github.com/greyli/bootstrap-flask',
     license='MIT',
     author='Grey Li',
