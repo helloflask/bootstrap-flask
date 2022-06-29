8aaffd8ad5019fe04f593f586c6aef22a8ab607f
Pander <pander@users.sourceforge.net>
2022-06-29 15:32:20 +0200

    improved examples with autocapitalization (#189)
    * improved examples with autocapitalization
    
    * example for month added

--- examples/bootstrap5/app.py
+++ examples/bootstrap5/app.py
@@ -34,20 +34,21 @@ class ExampleForm(FlaskForm):
     datetime = DateTimeField(render_kw={'placeholder': 'this is a placeholder'})  # add HTML attribute with `render_kw`
     datetime_local = DateTimeLocalField()
     time = TimeField()
+    month = MonthField()
     floating = FloatField()
     integer = IntegerField()
     decimal_slider = DecimalRangeField()
     integer_slider = IntegerRangeField(render_kw={'min': '0', 'max': '4'})
     email = EmailField()
     url = URLField()
-    search = SearchField()
     telephone = TelField()
     image = FileField(render_kw={'class': 'my-class'}, validators=[Regexp('.+\.jpg$')])  # add your class
     option = RadioField(choices=[('dog', 'Dog'), ('cat', 'Cat'), ('bird', 'Bird'), ('alien', 'Alien')])
     select = SelectField(choices=[('dog', 'Dog'), ('cat', 'Cat'), ('bird', 'Bird'), ('alien', 'Alien')])
     select_multiple = SelectMultipleField(choices=[('dog', 'Dog'), ('cat', 'Cat'), ('bird', 'Bird'), ('alien', 'Alien')])
     bio = TextAreaField()
-    title = StringField()
+    search = SearchField() # will autocapitalize on mobile
+    title = StringField() # will not autocapitalize on mobile
     secret = PasswordField()
     remember = BooleanField('Remember me')
     submit = SubmitField()
