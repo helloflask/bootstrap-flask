ccfae7b267315e6c77de86c982b24823fa00bee3
Grey Li <withlihui@gmail.com>
2023-10-11 21:56:28 +0800

    Work on 2.3.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='Bootstrap-Flask',
-    version='2.3.1',
+    version='2.3.2-dev',
     url='https://github.com/helloflask/bootstrap-flask',
     project_urls={
         'Documentation': 'https://bootstrap-flask.readthedocs.io/en/stable/',
