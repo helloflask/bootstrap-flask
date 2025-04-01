862e3a4e9554404534e0d6b0cb21ecc94c36ad12
pinguincoding <205418316+pinguincoding@users.noreply.github.com>
2025-04-01 20:07:31 +0200

    Fix icon font loading issue when BOOTSTRAP_SERVE_LOCAL is True
    This commit resolves an issue where the bootstrap-icons.min.css was not loading correctly when 'BOOTSTRAP_SERVE_LOCAL' was set to True.

--- flask_bootstrap/__init__.py
+++ flask_bootstrap/__init__.py
@@ -131,7 +131,7 @@ class _Bootstrap:
         """
         serve_local = current_app.config['BOOTSTRAP_SERVE_LOCAL']
         if serve_local:
-            icons_url = url_for('bootstrap.static', filename='font/bootstrap-icons.min.css')
+            icons_url = url_for('bootstrap.static', filename='css/font/bootstrap-icons.min.css')
         else:
             icons_url = f'{CDN_BASE}/bootstrap-icons@{self.icons_version}/font/bootstrap-icons.min.css'
         css = f'<link rel="stylesheet" href="{icons_url}">'
